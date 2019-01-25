fa3f24ed3a23fb79aed23e9c048fd01543f9dfa2
N13P4 <np_pr@protonmail.com>
2019-01-25 17:53:07 +0100

    Added description

--- src/project/additional/passwordgenerator.java
+++ src/project/additional/passwordgenerator.java
@@ -4,10 +4,10 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 public class passwordgenerator {
 
-    public String generatepassword(int length){
-
+    public static String generatepassword(int length) throws Exception{
+        // Characters used as a pool for generating a password => Your password exists of these chars.
         String pool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ0123456789!\"§$%&/()=?`´+*~#'-_.:,;<>|^[]{}\\€@";
-
+        //Generates the password | uses length from class MainWindow as Input
         String password = RandomStringUtils.random(length, pool);
 
         return password;
