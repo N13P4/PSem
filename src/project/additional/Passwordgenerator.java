e535f028fe0d85f30a37ebedc1e4674393036cc9
N13P4 <np_pr@protonmail.com>
2019-02-12 20:57:04 +0100

    Updated (final)

--- src/project/additional/Passwordgenerator.java
+++ src/project/additional/Passwordgenerator.java
@@ -2,6 +2,7 @@ package project.additional;
 
 
 import java.io.*;
+import org.jetbrains.annotations.NotNull;
 import project.Gui;
 import org.apache.commons.lang3.RandomStringUtils;
 import javax.swing.*;
@@ -11,20 +12,23 @@ public class Passwordgenerator {
 
     private static File f = new File("password_pool.cfg");
 
-    public static String execute(String length){
+
+    public static String execute(String length) throws IOException{
+
+        String pool;
         try{
             int l = Integer.parseInt(length);
             if(l < 1){
                 JOptionPane.showMessageDialog(Gui.popup,"Length must be greater than 0.");
             }else{
                 if(f.exists() && f.length() != 0){
-                    return generatepassword(l);
+                    return getString(l);
                 }else{
                     JOptionPane.showMessageDialog(Gui.popup, "Either there was no config file or it was empty.\nDefault pool was stored in \"password_pool.cfg\" edit this file to edit the password pool.");
                     FileWriter fw = new FileWriter("password_pool.cfg");
                     fw.write("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"§$%&/()=?´`+*~#'-_.:,;\\}][{€<>");
                     fw.close();
-                    generatepassword(l);
+                    return getString(l);
                 }
             }
         }catch(Exception e){
@@ -33,15 +37,12 @@ public class Passwordgenerator {
         return "error";
     }
 
-
-
-
-    private static String generatepassword(int length) throws IOException, NumberFormatException {
+    @NotNull
+    private static String getString(int l) throws IOException {
         String pool;
         FileReader fr = new FileReader("password_pool.cfg");
         BufferedReader br = new BufferedReader(fr);
         pool = br.readLine();
-        return RandomStringUtils.random(length, pool);
-
+        return RandomStringUtils.random(l, pool);
     }
 }
